6b98294d7002242617ac0d20fa45a1c416ca4662
Ella Choy <egal21de@gmail.com>
2021-10-20 00:52:56 +0200

    footer_update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,37 +6,39 @@ import logofooter from '../../assets/img/footer-logo.svg';
 
 const Footer= () => {
     return ( 
-<section className="footer_up">
-    <div className="collabo">
-        <img src={logofooter} classname="footer_logo" alt="" />
-        <h4 className="connect">We collaborate with ambitious<br/> brands and people. <span>Let’s connect:</span>
-        </h4>
-    </div>
-    <div className="contact">
-     <ul className="stay">
-         ⚪️ STAY IN THE KNOW
-         <li>Email Adress</li>
-         <li></li>
-     </ul>
-     <ul className="contact_info"> Social
-         <li><a href="#">Instagram </a></li>
-         <li><a href="#">Twitter</a></li>
-         <li><a href="#">Linkedin</a></li>
-         <li><a href="#">Facebook</a></li>
-     </ul>
-     <ul> INITIATIVES
-         <li><a href="#">Crafted  </a></li>
-         <li><a href="#">Culture Manual</a></li>
-         <li><a href="#">Applied </a></li>
-         <li><a href="#">Brandbeats</a></li>
-         <li><a href="#">Moves</a></li>
-     </ul>
-     <ul> OFFICES
-         <li><a href="#">San Diego – CA</a></li>
-         <li><a href="#">Bay Area – CA</a></li>
-         <li><a href="#">St. Louis – MO</a></li>
-     </ul>
+<section className="footer">
+    <div className="footer_upper">
+        <div className="collabo">
+            <img src={logofooter} classname="footer_logo" alt="" />
+            <h4 className="connect">We collaborate with ambitious<br/> brands and people. <span>Let’s connect:</span>
+            </h4>
+        </div>
+        <div className="contact">
+        <ul className="stay">
+            ⚪️ STAY IN THE KNOW
+            <li>Email Adress</li>
+            <li></li>
+        </ul>
+        <ul className="contact_info"> Social
+            <li><a href="#">Instagram </a></li>
+            <li><a href="#">Twitter</a></li>
+            <li><a href="#">Linkedin</a></li>
+            <li><a href="#">Facebook</a></li>
+        </ul>
+        <ul> INITIATIVES
+            <li><a href="#">Crafted  </a></li>
+            <li><a href="#">Culture Manual</a></li>
+            <li><a href="#">Applied </a></li>
+            <li><a href="#">Brandbeats</a></li>
+            <li><a href="#">Moves</a></li>
+        </ul>
+        <ul> OFFICES
+            <li><a href="#">San Diego – CA</a></li>
+            <li><a href="#">Bay Area – CA</a></li>
+            <li><a href="#">St. Louis – MO</a></li>
+        </ul>
 
+        </div>
     </div>
     <div className="footer_below">
         <p> BASIC®  INC 10 - 20©</p>
