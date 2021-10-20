aa5a0f775a5905fc4692440bf1e9bdf12a65daaf
Ella Choy <egal21de@gmail.com>
2021-10-20 18:28:34 +0200

    Featured

--- src/components/Featured/Featured.js
+++ src/components/Featured/Featured.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import google from '../../assets/img/google-dark.svg';
-import patagonia from '../../assets/img/patagonia-dark.svg';
-import Airbnb from '../../assets/img/airbnb-dark.svg';
+
+import BasicCompaniesData from '../../data/BasicCompanies.json'
 import './Featured.scss';
 
 // sfc
 const Featured = () => {
     return (
         <section className="companies">
-            <div className="com">
+            {BasicCompaniesData.map((basicCompanies,e)=> <div key={e} className="index">
+            <img src={basicCompanies.image} alt="com" className="companies_img"/>
+            <h5 clssName="com_name">{basicCompanies.title}</h5>
+            <p className="com_description">{basicCompanies.info}</p>
+            </div>
+
+
+        )}    
+        </section>
+    );
+}
+
+export default Featured;
+
+
+/* <div className="com">
                 <img src={google} className="img" alt="" />
                 <h5 clssName="com_name">GOOGLE</h5>
                 <p className="com_description">Our embedded partnership with Google is as deep as it gets. We’re the lead creative agency for Google Store and provide strategy, design, and prototyping to other divisions.</p>
@@ -22,15 +36,7 @@ const Featured = () => {
                 <img src={Airbnb}className="img"  alt="" />
                 <h5 clssName="com_name">AIRBNB</h5>
                 <p className="com_description">Helping the world’s leading peer-to-peer hospitality company with creative vision, experience strategy, and execution in partnership with various departments and divisions internally at Airbnb.</p>
-            </div>
-        </section>
-    );
-}
-
-export default Featured;
-
-
-
+            </div> */
 //  GOOGLE
 // Our embedded partnership with Google is as deep as it gets. We’re the lead creative agency for Google Store and provide strategy, design, and prototyping to other divisions.
 // PATAGONIA
