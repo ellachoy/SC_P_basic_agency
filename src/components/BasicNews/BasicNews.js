346883c63bc1bb931a5c4450f6236c1a70244250
Ella Choy <egal21de@gmail.com>
2021-10-21 14:29:48 +0200

    basic_news

--- src/components/BasicNews/BasicNews.js
+++ src/components/BasicNews/BasicNews.js
@@ -8,19 +8,20 @@ const BasicNews = () => {
     return (
         <div className="BasicNews">
             
-            <h2>FEATURED <br/> NEWS</h2>
+            <h2 id="basicnews_title">FEATURED <br/> NEWS</h2>
          
-                {BasicNewsData.map((basicnews,elt)=> <div key={elt} className="news_index">
-               <article clssName="news">
-                   <div className="news_container">
+                {BasicNewsData.map((basicnews,elt)=> <div key={elt} className="BasicNews">
+               <article className="news_container">
+                   
                     <img src={basicnews.image} alt="com" className="basicnews_img"/>
-                    </div>
-                    <div>
-                        <h2 clssName="com_name">{basicnews.title}</h2>
-                        <p className="line"></p>
+                  
+                    <div className="news_info">
+                        <h3 className="com_title">{basicnews.title}</h3>
+                       
                         <p className="com_description">{basicnews.info}
                         <span>{basicnews.date}</span></p>
                     </div>
+                    <img src={basicnews.arrow} alt="arrow" className="basicnews_arrow"/>
                 </article>
             </div>
            
