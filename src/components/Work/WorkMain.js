99e0d9882a105c4c57e24d842b9650e61b6846a6
Ella Choy <egal21de@gmail.com>
2021-10-25 15:23:27 +0200

    workMain

--- src/components/Work/WorkMain.js
+++ src/components/Work/WorkMain.js
@@ -7,6 +7,7 @@ import './WorkMain.scss';
 const WorkMain = () => {
     return (
         <div className="workpage">
+            <article className="workMain">
             {WorkMainData.map((data,elt)=> <div key={elt} className="work_page">
                 <div id="workdata_box_left">
                         <p className="workdata_date">
@@ -18,22 +19,28 @@ const WorkMain = () => {
                             {data.btn}
                         </button>
                 </div>
-                <div>
-                {/* <video src="/Google-Store.mp4"  controls autoplay loop id="google_mp"></video>
-                    <h5>Google Store</h5>
-                    <p>We go beyond best practices and build best-in-class D2C channels that drive commerce, shape culture, and define categories.</p> */}
+                <div id="workdata_box_right"> 
                     
-                    <figure>
-                            <img src={data.brandimages.image}alt="com" className="brand_img"/>
+                    <figure id="left">
+                 
+                        <img src={data.brandimages[0].image}alt="com" className="brand_img"/>
+                        <figurecaption> 
+                        <h5 className="brand_title">{data.brandimages[0].title}</h5>
+                        <p className="brand_info">{data.brandimages[0].info}</p>
+                        </figurecaption>
+                    </figure>  
+                    <figure id="right">
+                            <img src={data.brandimages[1].image}alt="com" className="brand_img"/>
                             <figurecaption> 
-                            <h5 className="brand_title">{data.brandimages.title}</h5>
-                            <p className="brand_info">{data.brandimages.info}</p>
+                            <h5 className="brand_title">{data.brandimages[1].title}</h5>
+                            <p className="brand_info">{data.brandimages[1].info}</p>
                             </figurecaption>
-                        </figure>  
+                    </figure>      
                         
                 </div>
                
             </div>)}
+            </article>
             
 
         </div>
